8c5ef16aa391254b3b15b7dfcbacedd47d575610
ginty1 <chiombagintykondwani@gmail.com>
2021-06-18 20:42:30 +0200

    user signin state added

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,10 +1,18 @@
+import { SET_USER } from "../actions/actionType";
 const INITIAL_STATE = {
   user: null,
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case SET_USER:
+      return {
+        ...state,
+        //adding user info to the default state
+        user: action.user,
+      };
     default:
+      //returnig the default state
       return state;
   }
 };
